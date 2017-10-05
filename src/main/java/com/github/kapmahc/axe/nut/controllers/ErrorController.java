d2e7bc0e3516f4b291e230623c35bb6336f2360b
Jeremy Zheng <jitang.zheng@gmail.com>
2017-10-05 12:55:46 -0700

    /error

---
+++ src/main/java/com/github/kapmahc/axe/nut/controllers/ErrorController.java
@@ -0,0 +1,82 @@
+package com.github.kapmahc.axe.nut.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Controller
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+
+
+    public ErrorController(ErrorAttributes errorAttributes) {
+        this.errorAttributes = errorAttributes;
+    }
+
+    /**
+     * Supports the HTML Error View
+     */
+    @RequestMapping(value = ERROR_PATH, produces = "text/html")
+    public ModelAndView errorHtml(HttpServletRequest request) {
+        Map<String, Object> model = getErrorAttributes(request, getTraceParameter(request));
+        logger.debug("{}", model);
+        return new ModelAndView("/nut/error", model);
+    }
+
+    /**
+     * Supports other formats like JSON, XML
+     */
+    @RequestMapping(value = ERROR_PATH)
+    @ResponseBody
+    public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
+        Map<String, Object> body = getErrorAttributes(request, getTraceParameter(request));
+        HttpStatus status = getStatus(request);
+        return new ResponseEntity<>(body, status);
+    }
+
+    /**
+     * Returns the path of the error page.
+     */
+    @Override
+    public String getErrorPath() {
+        return ERROR_PATH;
+    }
+
+
+    private boolean getTraceParameter(HttpServletRequest request) {
+        return Boolean.parseBoolean(request.getParameter("trace"));
+    }
+
+    private Map<String, Object> getErrorAttributes(HttpServletRequest request,
+                                                   boolean includeStackTrace) {
+        return errorAttributes.getErrorAttributes(new ServletWebRequest(request),
+                includeStackTrace);
+    }
+
+    private HttpStatus getStatus(HttpServletRequest request) {
+        Integer statusCode = (Integer) request
+                .getAttribute("javax.servlet.error.status_code");
+        if (statusCode != null) {
+            try {
+                return HttpStatus.valueOf(statusCode);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        return HttpStatus.INTERNAL_SERVER_ERROR;
+    }
+
+    private ErrorAttributes errorAttributes;
+    private final static String ERROR_PATH = "/error";
+
+    private final static Logger logger = LoggerFactory.getLogger(ErrorController.class);
+}
