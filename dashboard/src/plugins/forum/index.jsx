9a3ffcce30a2470e19354575703e6da9f8ca226d
Jeremy Zheng <jitang.zheng@gmail.com>
2017-10-30 17:25:29 -0700

    /forum/comments

--- dashboard/src/plugins/forum/index.jsx
+++ dashboard/src/plugins/forum/index.jsx
@@ -5,6 +5,8 @@ import IndexTags from './tags/Index'
 import FormTag from './tags/Form'
 import IndexArticles from './articles/Index'
 import FormArticle from './articles/Form'
+import IndexComments from './comments/Index'
+import FormComment from './comments/Form'
 
 const routes = [
   (< Route key = "forum.tags.edit" path = "/forum/tags/edit/:id" component = {
@@ -16,6 +18,7 @@ const routes = [
   (< Route key = "forum.tags.index" path = "/forum/tags" component = {
     IndexTags
   } />),
+
   (< Route key = "forum.articles.edit" path = "/forum/articles/edit/:id" component = {
     FormArticle
   } />),
@@ -24,6 +27,16 @@ const routes = [
   } />),
   (< Route key = "forum.articles.index" path = "/forum/articles" component = {
     IndexArticles
+  } />),
+
+  (< Route key = "forum.comments.edit" path = "/forum/comments/edit/:id" component = {
+    FormComment
+  } />),
+  (< Route key = "forum.comments.new" path = "/forum/comments/new/:articleId" component = {
+    FormComment
+  } />),
+  (< Route key = "forum.comments.index" path = "/forum/comments" component = {
+    IndexComments
   } />)
 ]
 
