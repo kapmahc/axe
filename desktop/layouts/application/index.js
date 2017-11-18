d4ad3c73758d6df6060c8e46cb7bfdea9564f9f0
Jeremy Zheng <jitang.zheng@gmail.com>
2017-11-17 17:05:25 -0800

    fix run script bug

---
+++ desktop/layouts/application/index.js
@@ -0,0 +1,7 @@
+import Header from './Header'
+import Footer from './Footer'
+
+export default({children}) => (<div>
+  <Header/> {children}
+  <Footer/>
+</div>)
--- desktop/pages/index.js
+++ desktop/pages/index.js
@@ -1,1 +1,5 @@
-export default() => <div>Welcome to next.js!</div>
+import Layout from '../layouts/application'
+
+export default() => (<Layout>
+  <div>Welcome to next.js1!</div>
+</Layout>)
